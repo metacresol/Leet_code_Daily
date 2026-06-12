8960230adde30431e64c2759519c2d360be78d42
Rajveer Singh <learnerrajveer.29@gmail.com>
2026-06-12 16:21:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (42.33%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long binaryform=n;
+        if(n<0){
+            x=1/x;
+            binaryform=-binaryform;
+        }
+        double ans=1.0;
+        while(binaryform>0){
+            if(binaryform%2==1)
+                ans*=x;
+            x*=x;
+            binaryform/=2;
+        }
+        return ans;
+    }
+};
