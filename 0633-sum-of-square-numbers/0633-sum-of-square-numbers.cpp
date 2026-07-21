8b4f8e6917cf9d2a86659711b566bb12e52c1e41
Rajveer Singh <learnerrajveer.29@gmail.com>
2026-07-21 23:32:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (57.15%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long l = 0;
+        long long r = sqrt(c);
+
+        while (l <= r) {
+            long long sum = l * l + r * r;
+
+            if (sum == c)
+                return true;
+            else if (sum < c)
+                l++;
+            else
+                r--;
+        }
+
+        return false;
+    }
+};
